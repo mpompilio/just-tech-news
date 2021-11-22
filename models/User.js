71186c2e5f0f2e6863cd50b2bf9f173b5c49de69
Matthew Pompilio <mattflynnpomp@aol.com>
2021-11-22 12:17:06 -0500

    added a message

--- models/User.js
+++ models/User.js
@@ -1,14 +1,22 @@
-const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
 
 // define table columns and configuration
-User.init(
-    {
-      // define an id column
-      id: {
+User.init({
+    // define an id column
+    id: {
         // use the special Sequelize DataTypes object provide what type of data it is
         type: DataTypes.INTEGER,
         // this is the equivalent of SQL's `NOT NULL` option
@@ -17,40 +25,50 @@ User.init(
         primaryKey: true,
         // turn on auto increment
         autoIncrement: true
-      },
-      // define a username column
-      username: {
+    },
+    // define a username column
+    username: {
         type: DataTypes.STRING,
         allowNull: false
-      },
-      // define an email column
-      email: {
+    },
+    // define an email column
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
         // there cannot be any duplicate email values in this table
         unique: true,
         // if allowNull is set to false, we can run our data through validators before creating the table data
         validate: {
-          isEmail: true
+            isEmail: true
         }
-      },
-      // define a password column
-      password: {
+    },
+    // define a password column
+    password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          // this means the password must be at least four characters long
-          len: [4]
+            // this means the password must be at least four characters long
+            len: [4]
         }
-      }
-    },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
     }
-  );
+}, {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+});
 
 module.exports = User;
